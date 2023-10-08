7bbf241896f5cd4446f4c01ec20d5f510fd1e3f7
mehmetKargaci <mehmetkargaci34@gmail.com>
2023-10-08 18:55:47 +0200

    Initial Commit

---
+++ TecnologyNews/TecnologyNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TecnologyNewsApp.swift
+//  TecnologyNews
+//
+//  Created by Mehmet on 08.10.23.
+//
+
+import SwiftUI
+
+@main
+struct TecnologyNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
